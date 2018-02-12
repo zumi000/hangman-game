96035a90e2c7240ca289f8767b7906d1d55dee44
révész tamás <revesz@ta.mas>
2018-02-12 14:16:15 +0100

    matrixra alakitva

--- hang.js
+++ hang.js
@@ -1,56 +1,61 @@
 const readlineSync = require('readline-sync');
 show = require('./graphics');
+guess = require('./guessing');
 
-var quiz = "text of the quiz";
-var playerName = "test";
 console.log('\x1Bc');
-// playerName = readlineSync.question('Kérlek, add meg a nevedet: ');
+playerName = readlineSync.question('Kérlek, add meg a nevedet: ');
 function enterMenu(playerName) {
-    console.log('\x1Bc');
-    show.logo();
-    console.log('Üdvözöllek, ' + playerName + "!");
-    show.menu();
-    return mainMenu(playerName);
+  console.log('\x1Bc');
+  show.logo();
+  console.log('Üdvözöllek, ' + playerName + "!");
+  show.menu();
+  return mainMenu(playerName);
 }
 
 function mainMenu (playerName) {
-    var menu = readlineSync.question('A választott menüpont: ');
-    switch(menu) {
-        case '1':
-            //STARTJATEK
-	    console.log('\x1Bc');
-	    show.logo();
-  	    console.log(playerName + " játékban");
-	    show.phase0(playerName);
-            return console.log("A feladvány: " + quiz);
-        case '2':
-            //TEMAKOR
-            return show.phase5(playerName);
-        case '3':
-            //NEHEZSEG
-            return show.phase4(playerName);
-        case '4':
-            //SCORE
-            return show.phase3(playerName);
-        case '5':
-            return  console.log("Köszönjük, hogy velünk játszottál, várunk vissza");
-        default:
-            return wrongMenu();
-    }
+  var menu = readlineSync.question('A választott menüpont: ');
+  switch (menu) {
+    case '1':
+      //STARTJATEK
+      console.log('\x1Bc');
+      show.logo();
+      console.log(playerName + " játékban");
+      show.phase0(playerName);
+      guess.set();
+      guess.loop ();
+      break;
+    case '2':
+      //TEMAKOR
+      show.phase0(playerName);
+      break;
+    case '3':
+      //NEHEZSEG
+      show.phase1(playerName);
+      show.matrix();
+      break;
+    case '4':
+      //SCORE
+      show.phase3(playerName);      
+      break;
+    case '5':
+      console.log("Köszönjük, hogy velünk játszottál, várunk vissza");
+    default:
+      wrongMenu();
+      break;
 }
+}
+
 function showQuiz() {
-    console.log(quiz);
+  console.log(quiz);
 }
 
 function wrongMenu() {
-    console.log('\x1Bc');
-    show.logo();
-    console.log('Üdvözöllek, ' + playerName + "!");
-    show.menu();
-    console.log("Nincs ilyen menüpont");
-    mainMenu(playerName);
+  console.log('\x1Bc');
+  show.logo();
+  console.log('Üdvözöllek, ' + playerName + "!");
+  show.menu();
+  console.log("Nincs ilyen menüpont");
+  mainMenu(playerName);
 }
-
-
 
 enterMenu(playerName);
