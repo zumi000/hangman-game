56cd599264ab8770b38484d0ed11b40f17de7417
révész tamás <revesz@ta.mas>
2018-02-12 16:03:13 +0100

    guess upd && hangmantest upd

--- hangmantest.js
+++ hangmantest.js
@@ -1,11 +1,22 @@
-//var myArray = ['Bones', 'Psych', 'Big Bang Theory', 'Breaking Bad', 'Modern Family', 'Game of Thrones', 'Dexter'];
-
-import {hello} from './hangmansource.js'; // or './module'
-let val = hello(); // val is "Hello";
-
-// importálja be a hangmansource.txt fájl tartalmát myArray néven
-
-
-
-var rand = myArray[Math.floor(Math.random() * myArray.length)];
-console.log(rand);
+const readlineSync = require('readline-sync');var playerName = 'test';
+require('./hangmansource.js');
+/*
+var word = allatok[Math.floor(Math.random() * allatok.length)];
+console.log(word);
+*/
+var menu = readlineSync.question('válassz témát: ');
+switch (menu) {
+ case '1':
+   var word = film[Math.floor(Math.random() * film.length)];
+   console.log(word);
+   break;
+ case '2':
+   var word = allatok[Math.floor(Math.random() * allatok.length)];
+   console.log(word);
+   break;
+ case '3':
+   var word = autobrandek[Math.floor(Math.random() * autobrandek.length)];
+   console.log(word);
+   break;
+}
+console.log(word);
