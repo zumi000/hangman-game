a77f4a7baab4e0bd174d11a6de4c1450ebbe7f08
Papp Gergely Flórián <papp.gergely.florian@gmail.com>
2018-02-12 15:51:15 +0100

    guessing update

--- guessing.js
+++ guessing.js
@@ -1,38 +1,55 @@
 const readlineSync = require('readline-sync');
-var word = 'aaa def ghi';
+var word = 'bash script boys feat lady java';
 // var letter = readlineSync.question('Enter letter:');
 var errors = 0;
 var correctLetters = 0;
 var spaceCounter = 0;
 var checkChange = false;
-var array = [];
+var wordArray = [];
+var goodLetters = [];
+var wrongLetters = [];
 
 function setArray () {
   for (var i = 0; i < word.length; i++) {
     if (word[i] === ' ') {
-      array.push(' ');
+      wordArray.push(' ');
       spaceCounter++;
     } else {
-      array.push('_');
+      wordArray.push('_');
     }
   }
+}
+
+function checkRepetition (array, letter) {
+  var rep = false;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === letter) {
+      rep = true;
+    }
+  }
+  return rep;
 }
 
 function check (letter) {
-  for (var i = 0; i < word.length; i++) {
-    if (word.charAt(i) === letter) {
-      array[i] = letter;
-      checkChange = true;
-      correctLetters++;
+  if (!checkRepetition(goodLetters, letter) && !checkRepetition(wrongLetters, letter)) {
+    for (var i = 0; i < word.length; i++) {
+      if (word.charAt(i) === letter) {
+        wordArray[i] = letter;
+        checkChange = true;
+        correctLetters++;
+      }
     }
+    if (checkChange === false) {
+      errors++;
+      wrongLetters.push(letter);
+    } else {
+      goodLetters.push(letter);
+    }
+    checkChange = false;
   }
-  if (checkChange === false) {
-    errors++;
-  }
-  checkChange = false;
 }
 
-function logArray () {
+function logArray (array) {
   var row = ' ';
   for (var i = 0; i < array.length; i++) {
     row += array[i] + ' ';
@@ -43,11 +60,18 @@ function logArray () {
 function gameLoop () {
   while (correctLetters < word.length - spaceCounter) {
     var letter = readlineSync.question('Enter letter:');
+    console.log('\x1Bc');
+    console.log('Please do not enter a letter more than once.');
     check(letter);
-    logArray();
-    console.log(letter + ' ');
-    console.log(word.length);
-    console.log(spaceCounter);
+    console.log('The expression to be guessed:');
+    logArray(wordArray);
+    console.log('Correct letters: ');
+    logArray(goodLetters);
+    console.log('Incorrect letters: ');
+    logArray(wrongLetters);
+    // console.log(letter + ' ');
+    // console.log(word.length);
+    // console.log(spaceCounter);
     console.log(errors);
     console.log(correctLetters);
   }
@@ -56,3 +80,10 @@ function gameLoop () {
 
 setArray();
 gameLoop();
+
+/*
+module.exports = {
+  set: setArray,
+  loop: gameLoop
+};
+*/
