93ba6ea4d38496b9a9047ce3577787ce8ce095b0
révész tamás <revesz@ta.mas>
2018-02-12 11:16:41 +0100

    source added

--- hangmantest.js
+++ hangmantest.js
@@ -1,8 +1,11 @@
-var myArray = ['Bones', 'Psych', 'Big Bang Theory', 'Breaking Bad', 'Modern Family', 'Game of Thrones', 'Dexter'];
-
-var rand = myArray[Math.floor(Math.random() * myArray.length)];
-console.log(rand);
-
+//var myArray = ['Bones', 'Psych', 'Big Bang Theory', 'Breaking Bad', 'Modern Family', 'Game of Thrones', 'Dexter'];
 
 import {hello} from './hangmansource.js'; // or './module'
 let val = hello(); // val is "Hello";
+
+// importálja be a hangmansource.txt fájl tartalmát myArray néven
+
+
+
+var rand = myArray[Math.floor(Math.random() * myArray.length)];
+console.log(rand);
