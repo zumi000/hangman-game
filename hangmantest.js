bc8b2358d7c3064f2adffc057c1cffe41e8c6ad0
Attila Nagy <attila.nagy880@gmail.com>
2018-02-13 12:45:55 +0100

    extended category menu

--- hangmantest.js
+++ hangmantest.js
@@ -1,27 +1,35 @@
 const readlineSync = require('readline-sync');
 
-film = ['bones', 'anal intruder', 'psych', 'big bang theory', 'breaking bad', 'modern family', 'game of thrones', 'dexter'];
-animals = ['great white shark', 'butterfly', 'grizzly bear', 'fallow deer'];
-manufacturers = ['ford', 'mercedes benz', 'jaguar'];
-
+let film = ['bones', 'anal intruder', 'psych', 'big bang theory', 'breaking bad', 'modern family', 'game of thrones', 'dexter'];
+let animals = ['great white shark', 'butterfly', 'grizzly bear', 'fallow deer'];
+let manufacturers = ['ford', 'mercedes benz', 'jaguar', 'caldwell vale', 'sichuan tengzhong', 'lamborghini', 'land rover'];
+let musicartists = ['jimi hendrix', 'elvis presley', 'david bowie', 'johnny cash', 'micheal jackson', 'billie holiday', 'amy winehouse'];
+let actors = ['jack nicholson', 'al pacino', 'dustin hoffman', 'meryl streep', 'julia roberts', 'nicole kidman'];
 function categorySelect () {
   console.log('1. Film Titles');
   console.log('2. Animals');
   console.log('3. Car Manufacturers');
+  console.log('4. Musicans');
+  console.log('5. Actors');
   var categoryMenu = readlineSync.question('Chose category: ');
   switch (categoryMenu) {
     case '1':
       return film[Math.floor(Math.random() * film.length)];
       break;
     case '2':
-      return manufacturers[Math.floor(Math.random() * manufacturers.length)];
+      return animals[Math.floor(Math.random() * animals.length)];
       break;
     case '3':
       return manufacturers[Math.floor(Math.random() * manufacturers.length)];
       break;
+    case '4':
+      return musicartists[Math.floor(Math.random() * musicartists.length)];
+      break;
+    case '5':
+      return actors[Math.floor(Math.random() * actors.length)];
+      break;
   }
 }
-
 module.exports = {
   categorySelect: categorySelect
 };
