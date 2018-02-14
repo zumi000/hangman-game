0613073aa1e0a781f89959f7e0f6a0ff75efde80
Papp Gergely Flórián <papp.gergely.florian@gmail.com>
2018-02-14 11:30:57 +0100

    fix

--- hangmantest.js
+++ hangmantest.js
@@ -1,15 +1,15 @@
 const readlineSync = require('readline-sync');
 
-let film = ['anal intruder', 'big bang theory', 'breaking bad', 'game of thrones'];
-let harderFilm = ['bones', 'psych', 'modern family', 'dexter'];
+let film = ['Anal Intruder', 'Big Bang Theory', 'Breaking Bad', 'Game of Thrones'];
+let harderFilm = ['Bones', 'Psych', 'Modern Family', 'Dexter'];
 let animals = ['great white shark', 'butterfly', 'grizzly bear', 'fallow deer'];
 let harderAnimals = ['great white shark', 'butterfly', 'grizzly bear', 'fallow deer'];
-let manufacturers = ['ford', 'mercedes benz', 'jaguar', 'caldwell vale', 'sichuan tengzhong', 'lamborghini', 'land rover'];
-let harderManufacturers = ['ford', 'mercedes benz', 'jaguar', 'caldwell vale', 'sichuan tengzhong', 'lamborghini', 'land rover'];
-let musicans = ['jimi hendrix', 'elvis presley', 'david bowie', 'johnny cash', 'micheal jackson', 'billie holiday', 'amy winehouse'];
-let harderMusicans = ['jimi hendrix', 'elvis presley', 'david bowie', 'johnny cash', 'micheal jackson', 'billie holiday', 'amy winehouse'];
-let actors = ['jack nicholson', 'al pacino', 'dustin hoffman', 'meryl streep', 'julia roberts', 'nicole kidman'];
-let harderActors = ['jack nicholson', 'al pacino', 'dustin hoffman', 'meryl streep', 'julia roberts', 'nicole kidman'];
+let manufacturers = ['Ford', 'Mercedes Benz', 'Jaguar', 'Caldwell Vale', 'Sichuan Tengzhong', 'Lamborghini', 'Land Rover'];
+let harderManufacturers = ['Ford', 'Mercedes Benz', 'Jaguar', 'Caldwell Vale', 'Sichuan Tengzhong', 'Lamborghini', 'Land Rover'];
+let musicans = ['Jimi Hendrix', 'Elvis Presley', 'David Bowie', 'Johnny Cash', 'Micheal Jackson', 'Billie Holiday', 'Amy Winehouse'];
+let harderMusicans = ['Jimi Hendrix', 'Elvis Presley', 'David Bowie', 'Johnny Cash', 'Micheal Jackson', 'Billie Holiday', 'Amy Winehouse'];
+let actors = ['Jack Nicholson', 'Al Pacino', 'Dustin Hoffman', 'Meryl Streep', 'Julia Roberts', 'Nicole Kidman'];
+let harderActors = ['Jack Nicholson', 'Al Pacino', 'Dustin Hoffman', 'Meryl Streep', 'Julia Roberts', 'Nicole Kidman'];
 function categorySelect () {
   console.log('1. Film Titles');
   console.log('2. Animals');
@@ -20,19 +20,14 @@ function categorySelect () {
   switch (categoryMenu) {
     case '1':
       return film[Math.floor(Math.random() * film.length)];
-      break;
     case '2':
       return animals[Math.floor(Math.random() * animals.length)];
-      break;
     case '3':
       return manufacturers[Math.floor(Math.random() * manufacturers.length)];
-      break;
     case '4':
       return musicans[Math.floor(Math.random() * musicans.length)];
-      break;
     case '5':
       return actors[Math.floor(Math.random() * actors.length)];
-      break;
     default:
       break;
   }
@@ -47,19 +42,14 @@ function harderCategorySelect () {
   switch (categoryMenu) {
     case '1':
       return harderFilm[Math.floor(Math.random() * harderFilm.length)];
-      break;
     case '2':
       return harderAnimals[Math.floor(Math.random() * harderAnimals.length)];
-      break;
     case '3':
       return harderManufacturers[Math.floor(Math.random() * harderManufacturers.length)];
-      break;
     case '4':
       return harderMusicans[Math.floor(Math.random() * harderMusicans.length)];
-      break;
     case '5':
       return harderActors[Math.floor(Math.random() * harderActors.length)];
-      break;
     default:
       break;
   }
