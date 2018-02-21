7730fc2c9b934b914f49ac598609c6806c3cc1cc
Attila Nagy <attila.nagy880@gmail.com>
2018-02-21 12:07:53 +0100

    utolso utolso commit

--- hang.js
+++ hang.js
@@ -6,69 +6,74 @@ var word;
 var imHard = false;
 var fs = require('fs');
 console.log('\x1Bc');
-let playerName = readlineSync.question('Please, enter your name: ');
-enterMenu(playerName);
-function enterMenu (playerName) {
-  console.log('\x1Bc');
-  show.logo();
-  console.log('Welcome, ' + playerName + '!\n');
-  console.log('First you must to select category\n');
-  word = select.easy();
-  return mainMenu(playerName);
-}
-
-function selectChat (playerName) {
-  console.log('\x1Bc');
-  show.logo();
-  console.log('Welcome, ' + playerName + '!\n');
-  console.log('Here you can modify the selected category\n');
-  if (imHard == true) {
-    word = select.hard();
-  } else {
+function HangmanStart () {
+  let playerName = readlineSync.question('Please, enter your name: ');
+  enterMenu(playerName);
+  function enterMenu(playerName) {
+    console.log('\x1Bc');
+    show.logo();
+    console.log('Welcome, ' + playerName + '!\n');
+    console.log('First you must to select category\n');
     word = select.easy();
+    return mainMenu(playerName);
   }
-  return mainMenu(playerName);
-}
 
-function mainMenu (playerName) {
-  show.logo();
-  console.log('Welcome, ' + playerName + '!\n');
-  show.menu();
-  var menu = readlineSync.keyIn('Please select a menu: ', { limit: '$<1-5>' });
-  switch (menu) {
-    case '1':
-      // start game
-      console.log('\x1Bc');
-      guess.loop(word, playerName);
-      var newGame = readlineSync.keyIn('Do you want to start another game? [y/n]', {limit: '$<ynYN>'});
-      switch (newGame) {
-        case 'y': case 'Y':
-          enterMenu(playerName);
-          break;
-        case 'n': case 'N':
-          console.log('Thank you for choosing us');
-          process.exit();
-      }
-      break;
-    case '2':
-      // modify category
-      selectChat(playerName);
-      break;
-    case '3':
-      // modify difficulty
-      imHard = !imHard;
-      selectChat(playerName);
-      break;
-    case '4':
-      // scorecard
-      console.log('\x1Bc');
-      console.log('Username ' + 'Expression ' + 'Length');
-      var data = fs.readFileSync('results.txt');
-      console.log(data.toString());
-      readlineSync.keyIn('Please hit a key to return to the main menu.');
-      return mainMenu(playerName);
-    case '5':
-      console.log('Thank you for choosing us');
-      process.exit();
+  function selectChat(playerName) {
+    console.log('\x1Bc');
+    show.logo();
+    console.log('Welcome, ' + playerName + '!\n');
+    console.log('Here you can modify the selected category\n');
+    if (imHard == true) {
+      word = select.hard();
+    } else {
+      word = select.easy();
+    }
+    return mainMenu(playerName);
+  }
+
+  function mainMenu(playerName) {
+    show.logo();
+    console.log('Welcome, ' + playerName + '!\n');
+    show.menu();
+    var menu = readlineSync.keyIn('Please select a menu: ', { limit: '$<1-5>' });
+    switch (menu) {
+      case '1':
+        // start game
+        console.log('\x1Bc');
+        guess.loop(word, playerName);
+        var newGame = readlineSync.keyIn('Do you want to start another game? [y/n]', { limit: '$<ynYN>' });
+        switch (newGame) {
+          case 'y': case 'Y':
+            enterMenu(playerName);
+            break;
+          case 'n': case 'N':
+            console.log('Thank you for choosing us');
+            process.exit();
+        }
+        break;
+      case '2':
+        // modify category
+        selectChat(playerName);
+        break;
+      case '3':
+        // modify difficulty
+        imHard = !imHard;
+        selectChat(playerName);
+        break;
+      case '4':
+        // scorecard
+        console.log('\x1Bc');
+        console.log('Username ' + 'Expression ' + 'Length');
+        var data = fs.readFileSync('results.txt');
+        console.log(data.toString());
+        readlineSync.keyIn('Please hit a key to return to the main menu.');
+        return mainMenu(playerName);
+      case '5':
+        console.log('Thank you for choosing us');
+        process.exit();
+    }
   }
 }
+module.exports = {
+  HangmanStart: HangmanStart
+};
